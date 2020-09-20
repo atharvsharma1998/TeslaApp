10e055d9f1f6ee3f78cf1c20e64f71f76f2f55e7
Atharv Sharma <atharv228.as@gmail.com>
2020-09-20 16:23:34 +0530

    Initial Commit

---
+++ TeslaNew/TeslaNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaNewApp.swift
+//  TeslaNew
+//
+//  Created by Atharv Sharma on 20/09/20.
+//
+
+import SwiftUI
+
+@main
+struct TeslaNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
